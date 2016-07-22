beb56259afe64b86d22cb676da5aea7b457575aa
dfenjves <fenjves.daniel@gmail.com>
2016-07-22 13:01:52 -0400

    done

--- application_controller.rb
+++ application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/no' do
+    @no = ["nah", "nope", "no way", "nein", "no", "nay", "not a chance", "nice try, but no", "you're making me laugh", "never"].sample
+    @image = ["jeff.jpg", "jeff.jpg", "jeff2.jpg", "jeff3.jpg", "jeff4.jpg", "jeff5.jpg", "jeff6.jpg", "jeff7.jpg"].sample
+    erb :results
+  end
+
 end
